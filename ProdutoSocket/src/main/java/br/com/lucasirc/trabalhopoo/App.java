b24478e4cfd8707bd498ca86240f51c454842a18
Lucas Castro <lucasirc@gmail.com>
2012-12-03 20:46:15 -0200

    adicionando projeto maven

---
+++ ProdutoSocket/src/main/java/br/com/lucasirc/trabalhopoo/App.java
@@ -0,0 +1,13 @@
+package br.com.lucasirc.trabalhopoo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
